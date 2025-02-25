0c6ff9ed9a9257c6a7fb76dee701fdc2fccaeed3
memoriessss <117206004+memoriessss@users.noreply.github.com>
2025-02-24 20:57:01 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,5 @@ HEADERS = {"Content-Type": "application/json"}
 BLOCKS_PER_HOUR = 1800    # approximate for Base (adjust if needed)
 NUM_HOURS       = 5       # how many hourly chunks to want?
 
-# If token is token0 in the pool, set this to True
-# TOKEN_IS_TOKEN0 = False
-
 # Uniswap V3 "Swap" event signature (Keccak-256 of Swap(...) )
 SWAP_TOPIC = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67"
